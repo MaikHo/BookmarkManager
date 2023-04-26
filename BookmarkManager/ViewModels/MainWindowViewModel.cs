ee9ad05ab7d43f699149092f7f9cb1b27727e00c
Maik Hoffmann <maikhoffmann@outlook.com>
2023-04-26 11:14:13 +0200

    start Project on GitHub

---
+++ BookmarkManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Avalonia;
+
+namespace BookmarkManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public MainWindowViewModel()
+        {
+            //await Application.Current.Clipboard.SetTextAsync("Hello World!");
+
+            //var text = await Application.Current.Clipboard.GetTextAsync();
+            SaveUrlCommand = ReactiveCommand.Create(async () =>
+            {
+                // Code here will be executed when the button is clicked.
+                var text = await Application.Current.Clipboard.GetTextAsync();
+                text.ToString();
+            });
+
+            /*
+             SaveUrlCommand = ReactiveCommand.Create(() =>
+            {
+                // Code here will be executed when the button is clicked.                
+            });
+             */
+        }
+
+        public ICommand SaveUrlCommand { get; }
+
+        
+    }
+}
